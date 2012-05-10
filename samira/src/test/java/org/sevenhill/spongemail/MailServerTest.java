123c19dbc1eee6a2764a3b7e56d63158ec672fc5
tuoyo <eyituoyo@yahoo.com>
2012-05-10 06:46:50 -0400

    Mail server test class

--- samira/src/test/java/org/sevenhill/spongemail/MailServerTest.java
+++ samira/src/test/java/org/sevenhill/spongemail/MailServerTest.java
@@ -1,15 +1,42 @@
 package org.sevenhill.spongemail;
 
-import org.junit.Test;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import com.dumbster.smtp.SimpleSmtpServer;
+import org.junit.Test;
 
 public class MailServerTest {
 
 	@Test
 	public void hello() throws Exception {
-		SimpleSmtpServer server = SimpleSmtpServer.start(4000);
-		Thread.sleep(5000);
-		server.stop();
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				System.out.println("I am TimerTask, and I do nothing!");
+				
+			}
+		}, new Date());
+//		timer.scheduleAtFixedRate(new TimerTask() {
+//
+//			@Override
+//			public void run() {
+//				System.out.println("I am TimerTask, and I live!");
+//
+//			}
+//		}, new Date(System.currentTimeMillis() + 2000), 1500);
+		//
+		// Thread.sleep(10000);
+		// timer.cancel();
+		for (int i = 0; i < 5; i++) {
+			Thread.sleep(1000);
+			System.out.println("Cancelling the timer");
+			timer.cancel();
+		}
+		// SimpleSmtpServer server = SimpleSmtpServer.start(4000);
+		// Thread.sleep(5000);
+		// server.stop();
 	}
 }
